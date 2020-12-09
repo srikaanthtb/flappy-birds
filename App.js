7fa9dc1babda8dcb1a6f200d36f5803bb43b1eb6
srikaanthtb <srikaanth.t.b@gmail.com>
2020-12-09 16:37:58 +0000

    bird component created

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { Dimensions, StyleSheet, Text, View, } from 'react-native';
+import Bird from './components/Bird'
 export default function App() {
+
+  const screenWidth = Dimensions.get("screen").width
+  const screenHeight = Dimensions.get("screen").height
+  const birdLeft = screenWidth / 2
+  const [BirdBottom, setBirdBottom] = useState(screenHeight/2)
+
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>butts</Text>
+      <Bird />
     </View>
   );
 }
