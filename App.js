4d422c4dc428b6f7ffb3d6c9b1a4291dcabf47d4
srikaanthtb <srikaanth.t.b@gmail.com>
2020-12-10 17:51:13 +0000

    added obstacle

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import { Dimensions, StyleSheet, Text, View, } from 'react-native';
 import Bird from './components/Bird'
+import Obstacles from './components/Obstacles';
 export default function App() {
 
   const screenWidth = Dimensions.get("screen").width
   const screenHeight = Dimensions.get("screen").height
   const birdLeft = screenWidth / 2
   const [birdBottom, setbirdBottom] = useState(screenHeight/2)
+  const [obstaclesLeft, setobstaclesLeft] = useState(screenWidth)
+  const obstacleWidth = 60
+  const obstacleHeight = 300
+  const gap = 50
   const gravity = 3
   let gameTimerId
-
+  let obstaclesLeftTimerId
 
  useEffect(() => {
   if( birdBottom > 0){
@@ -25,13 +30,34 @@ export default function App() {
  }, [birdBottom])
 console.log(birdBottom)
 
+useEffect(() => {
+  if (obstaclesLeft > -obstacleWidth) {
+    obstaclesLeftTimerId = setInterval(() => {
+      setobstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+    }, 30)
+  }
+
+  return () => {
+    clearInterval(obstaclesLeftTimerId)
+  }
+
+}, [obstaclesLeft])
+
+
   return (
     <View style={styles.container}>
       <Bird
         birdBottom={birdBottom}
         birdLeft={birdLeft}
       />
+          <Obstacles
+    obstacleWidth={obstacleWidth}
+    obstacleHeight={obstacleHeight}
+    gap={gap}
+    obstaclesLeft={obstaclesLeft}
+    />
     </View>
+
   );
 }
 
